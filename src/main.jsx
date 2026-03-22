65034666affa593c35629105119b93ab97044343
ADARXZSH <chauhanadarsh502@gmail.com>
2026-03-22 04:20:05 -0700

    add speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<><App /><SpeedInsights /></>)
